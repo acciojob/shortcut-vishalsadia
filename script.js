0e6d419ff53c84edfcdad1578efad97a4a9e25fa
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-14 11:10:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function shortcut(s1, s2) {
   // your code here
+	    if not s1 or not s2:  
+        return ''
+
+   // # Return the initial letters of the strings
+    return s1[0].upper() + s2[0].lower()
 }
 
-// Do not change the code below.
+// // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
+ 
